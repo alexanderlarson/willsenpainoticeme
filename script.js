db14532ab04e17da2cfc513f697851af8e6ce485
Alexander Larson <alexanderlarson@gmail.com>
2014-01-12 15:22:35 -0600

    js, css positioning, normalize

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('h1').css({ 'width':'100%', 'text-align':'center' });
+  var h1 = $('h1').height();
+  var h = h1/2;
+  var w1 = $(window).height();
+  var w = w1/2;
+  var m = w - h
+  $('h1').css("margin-top",m + "px")
+});
